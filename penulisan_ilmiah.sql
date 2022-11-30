93b0585032100e598219102b693e7d790b658222
Ikhsan Nusaly <Ikhsanskuy>
2022-11-30 11:08:25 +0700

    added database

--- penulisan_ilmiah.sql
+++ penulisan_ilmiah.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:8889
--- Waktu pembuatan: 28 Nov 2022 pada 15.49
+-- Waktu pembuatan: 30 Nov 2022 pada 04.08
 -- Versi server: 5.7.34
 -- Versi PHP: 8.0.8
 
@@ -58,10 +58,32 @@ CREATE TABLE `news` (
 --
 
 INSERT INTO `news` (`id`, `nama`, `deskripsi`, `foto`, `timestamp`) VALUES
-(1, 'gunadarma', 'nrugqreiognqiognqiogrnerbuqbenbvoirgn', 'rio', '2022-11-28 12:13:07'),
+(1, 'nama', ' erqhifjqer', 'rio', '2022-11-28 21:51:05'),
 (2, 'ug tv', 'anjayiuerguerhijgrow', 'jjj', '2022-11-28 12:13:07'),
 (3, 'eroigj', 'eirngiongioqngiongr', 'nrfi', '2022-11-28 12:13:07'),
-(4, 'irg', 'ioerqngiongi', 'ierne', '2022-11-28 12:13:07');
+(5, 'tesik', 'ii  tes', 'room1.jpg', '2022-11-28 18:25:38');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `ukm`
+--
+
+CREATE TABLE `ukm` (
+  `id` int(11) NOT NULL,
+  `nama` varchar(255) NOT NULL,
+  `deskripsi` text NOT NULL,
+  `foto` varchar(255) NOT NULL,
+  `ig` varchar(100) DEFAULT NULL,
+  `twt` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data untuk tabel `ukm`
+--
+
+INSERT INTO `ukm` (`id`, `nama`, `deskripsi`, `foto`, `ig`, `twt`) VALUES
+(1, 'iuerg', '          erqheruuieqrwergewjaja', 'mapa1.jpg', 'https://www.instagram.com/cristiano', 'https://twitter.com/cristiano');
 
 --
 -- Indexes for dumped tables
@@ -80,6 +102,12 @@ ALTER TABLE `news`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indeks untuk tabel `ukm`
+--
+ALTER TABLE `ukm`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT untuk tabel yang dibuang
 --
 
@@ -87,7 +115,13 @@ ALTER TABLE `news`
 -- AUTO_INCREMENT untuk tabel `news`
 --
 ALTER TABLE `news`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT untuk tabel `ukm`
+--
+ALTER TABLE `ukm`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
