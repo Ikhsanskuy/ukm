b9023600cf5a0ee4c580a4390ba385ac4f09ea95
User <ikhsanusaly147@gmail.com>
2022-12-12 22:52:34 +0700

    added assets

--- index.php
+++ index.php
@@ -1,131 +1,229 @@
-<!DOCTYPE html>
+<?php
+include 'koneksi.php'; ?>
+<!doctype html>
 <html lang="en">
 
 <head>
-  <meta charset="UTF-8" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <title>UKM Universitas Gunadarma tes</title>
-  <link rel="stylesheet" href="css/bootstrap.min.css" />
-  <style>
-    .banner-image {
-      background-image: url('img/ukm3.jpg');
-      background-size: cover;
-    }
-  </style>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="icon" href="image/ug.png" type="image/png">
+    <title>UKM Gunadarma</title>
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="vendors/linericon/style.css">
+    <link rel="stylesheet" href="css/font-awesome.min.css">
+    <link rel="stylesheet" href="vendors/owl-carousel/owl.carousel.min.css">
+    <link rel="stylesheet" href="vendors/bootstrap-datepicker/bootstrap-datetimepicker.min.css">
+    <link rel="stylesheet" href="vendors/nice-select/css/nice-select.css">
+    <link rel="stylesheet" href="vendors/owl-carousel/owl.carousel.min.css">
+    <!-- main css -->
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/responsive.css">
+    <link href="css/carousel.css" rel="stylesheet">
+    <link href="js/bootstrap.min.css" rel="stylesheet">
 </head>
 
 <body>
-  <!-- Navbar  -->
-  <nav class="navbar fixed-top navbar-expand-lg navbar-dark p-md-3">
-    <div class="container">
-      <a class="navbar-brand" href="#">Universitas Gunadarma</a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
+    <!--================Header Area =================-->
+    <header class="header_area">
 
-      <div class="collapse navbar-collapse" id="navbarNav">
-        <div class="mx-auto"></div>
-        <ul class="navbar-nav">
-          <li class="nav-item">
-            <a class="nav-link text-white" href="index.php">Home</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link text-white" href="ukm.php">UKM</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link text-white" href="kontak.php">Kontak</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link text-white" href="login.php">Admin</a>
-          </li>
-        </ul>
-      </div>
-    </div>
-  </nav>
+        <div class="container">
+            <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+                <a class="navbar-brand logo_h" href="index.php"><img src="image/ug.png" width="60" height="60" alt=""></a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarCollapse">
+                    <ul class="navbar-nav ml-auto">
+                        <li class="nav-item">
+                            <a class="nav-link" href="#about">About</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#news">Post</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="ukm">UKM</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#contact">Contact</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
+    </header>
+    <!--================Header Area =================-->
 
-  <!-- Banner Image  -->
-  <div class="banner-image w-100 vh-100 d-flex justify-content-center align-items-center">
-    <div class="content text-center">
-      <h1 class="text-white">Unit Kegiatan Mahasiswa</h1>
-      <p class="text-white">Coloring the Global Future</p>
-    </div>
-  </div>
+    <!--================Banner Area =================-->
+    <section class="banner_area">
+        <div class="booking_table d_flex align-items-center">
+            <div class="overlay bg-parallax" data-stellar-ratio="0.9" data-stellar-vertical-offset="0" data-background=""></div>
+            <div class="container">
+                <div class="banner_content text-center">
+                    <h6>Away from monotonous life</h6>
+                    <h2>Relax Your Mind</h2>
+                    <p>If you are looking at blank cassettes on the web, you may be very confused at the<br> difference in price. You may see some for as low as $.17 each.</p>
+                    <a href="#" class="btn theme_btn button_hover">Get Started</a>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!--================Banner Area =================-->
 
-  <!-- Main Content Area -->
-  <div class="container my-5 d-grid gap-5">
-    <div class="border">
-      <h2 class="text-center"> Unit Kegiatan Mahasiswa </h2>
-      <p>
-        Unit Kegiatan Mahasiswa (UKM) adalah lembaga kemahasiswaan tempat berkumpulnya mahasiswa
-        yang memiliki kesamaan minat atau kegemaran, kreativitas, serta orientasi aktivitas penyaluran
-        kegiatan ekstrakurikuler di dalam kampus. Di bangku sekolah, istilah UKM ini biasanya dikenal dengan istilah ekskul atau ekstrakurikuler.
-        Tidak hanya berlandaskan pada azas manfaat semata, UKM juga berlandaskan pada azas edukatif dan memiliki nilai-nilai sosial
-        di antaranya: saling menghargai, gotong-royong, peduli terhadap sesama, kemandirian, persatuan dan kesatuan.
-      <p>
-        Universitas Gunadarma yang biasa disebut UG adalah salah satu Perguruan Tinggi Swasta terbaik di Indonesia.
-        Universitas Gunadarma termasuk salah satu perguruan tinggi swasta yang mempunyai taraf international, memiliki jejaring global dan tentu
-        saja dengan pendidikan tingi akan mampu menghasilkan lulusan terbaik. Universitas Gunadarma telah melalui perjalan yang relatif panjang
-        untuk menunjukkan bahwa Universitas Gunadarma telah berhasil menjadi yang terbaik. Selain itu Universitas Gunadarma kepemimpinan dan
-        sistem pengelolaannya mengacu kepada tata kelola universitas yang baik.
-        Universitas Gunadarma sendiri memiliki beberapa Unit Kegiatan Mahasiswa (UKM) yang masih berjalan sampai saat ini. Berikut ini adalah
-        nama-nama UKM di Universitas Gunadarma yang aktif sampai dengan saat ini.
-        <br>
-        <br>1. UKM Kerohanian Islam (UKM Rohis)
-        <br>2. UKM Kerohanian Kristen (UKM Rohkris)
-        <br>3. UKM Kerohanian Katolik (UKM Rohkat)
-        <br>4. UKM Kerohanian Hindu (UKM Rohin)
-        <br>5. UKM Kerohanian Budha (UKM Rohbud)
-        <br>6. UKM Rohani Khonghuchu (UKM RohKhong)
-        <br>7. UKM Paduan Suara (UKM PS)
-        <br>8. UKM Mahasiswa Pencinta Alam (UKM MAPA)
-        <br>
-      <h5>Lokasi Universitas Gunadarma</h5>
-      Universitas Gunadarma mempunyai program studi untuk Sarjana, Pascasarjana, dan Vokasi. Oleh karena itu,
-      Universitas Gunadarma mempunyai banyak gedung kampus yaitu sebanyak 13 kampus yang tersebar di Jakarta. Kampus utama
-      Universitas Gunadarma berada di Kampus D Kota Depok, Jawa Barat.
-      <br>
-      <br>1. Kampus A (Kampus Kenari di Jalan Kenari 3 Nomor 33, Jakarta Pusat)
-      <br>2. Kampus B (Kampus Salemba Bluntas di Jalan Salemba Bluntas Nomor 2, Jakarta Pusat)
-      <br>3. Kampuc C (Kampus Salemba di Jalan Salemba Raya Nomor 53, Jakarta Pusat)
-      <br>4. Kampus D (Kampus Depok di Jalan Margonda Raya Nomor 100, Depok)
-      <br>5. Kampus E (Kampus Kelapa Dua di Jalan Akses Kelapa 2, Depok)
-      <br>6. Kampus G dan H (Kampus Laboratorium Kelapa Dua di Jalan Akses Kelapa 2, Depok)
-      <br>7. Kampus H2 (Kampus Simatupang di Jalan Tahi Bonar Simatupang Kavling 38, Jakarta Selatan)
-      <br>8. Kampus J dan J1 (Jalan Kiai Haji Noer Ali, Bekasi)
-      <br>9. Kampus J3 (Kampus Kalimas di Jalan Inspeksi Saluran Kalimalang, Bekasi)
-      <br>10. Kampus J4/K (Kampus Kemang Pratama di Jalan Raya Kemang Pratama Blok MM No. 21, Bekasi)
-      <br>11. Kampus J5 (Kampus Cakung di Jalan Sentra Primer Baru Timur, Jakarta Timur)
-      <br>12. Kampus L (Kampus Cengkareng di Jalan Raya Kamal Outring No.75, Jakarta Barat)
-      <br>13. Kampus K (Jalan Danau Kelapa Dua, Tangerang)
-      </p>
-      <center>
-        <img src="img/mapa1.jpg" class="img-thumbnail" width="400px">
-        <img src="img/mapa2.jpg" class="img-thumbnail" width="400px">
-        <img src="img/mapa3.jpg" class="img-thumbnail" width="400px">
-      </center>
-    </div>
-  </div>
-  
-  <!-- footer -->
-  <div class="card text-center bg-dark">
-    <div class="card-body">
-      <p class="card-text text-white">Copyright &copy; 2022 - Zidan Walid Murtadho, All Rights
-        Reserved.</p>
-    </div>
-  </div>
+    <!--================ About History Area  =================-->
+    <section class="about_history_area section_gap" id="about">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6 d_flex align-items-center">
+                    <div class="about_content ">
+                        <h2 class="title title_color">About Us <br>Our History<br>Mission & Vision</h2>
+                        <p>inappropriate behavior is often laughed off as “boys will be boys,” women face higher conduct standards especially in the workplace. That’s why it’s crucial that, as women, our behavior on the job is beyond reproach. inappropriate behavior is often laughed.</p>
+                        <a href="#" class="button_hover theme_btn_two">Request Custom Price</a>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <img class="img-fluid" src="image/ug.png" alt="img" width="450" height="450">
+                </div>
+            </div>
+        </div>
+    </section>
+    <!--================ About History Area  =================-->
+
+    <!-- ================ Latest Blog Area  =================-->
+    <section class="facilities_area section_gap" id="news">
+        <div class="overlay bg-parallax" data-stellar-ratio="0.8" data-stellar-vertical-offset="0" data-background="">
+        </div>
+        <div class="container">
+            <div class="section_title text-center">
+                <h2 class="title_color text-white">latest posts from blog</h2>
+                <p>The French Revolution constituted for the conscience of the dominant aristocratic class a fall from </p>
+            </div>
+            <div class="row mb_30">
+
+                <?php $ambil = $conn->query("SELECT * FROM news "); ?>
+                <?php while ($news = $ambil->fetch_assoc()) { ?>
+
+                    <div class="col-lg-4 col-md-6 text-center">
+                        <div class="single-recent-blog-post">
+                            <div class="thumb">
+                                <img class="img-fluid" src="image/news/<?= $news['foto'] ?>" alt="post" style="border-radius:10%">
+                            </div>
+                            <div class="details text-white">
+                                <a href="news/index.php?id=<?= $news['id'] ?>">
+                                    <h4 class="sec_h4 text-white"><?php echo $news['nama']; ?></h4>
+                                </a>
+                                <p><?php echo $news['deskripsi']; ?></p>
+                                <h6 class="date title_color text-white"><?php echo $news['timestamp']; ?></h6>
+                            </div>
+                        </div>
+                    </div>
+                <?php } ?>
+            </div>
+        </div>
+    </section>
+    <!--================ Recent Area  ================= -->
 
 
-  <script src="js/bootstrap.bundle.min.js"></script>
-  <script type="text/javascript">
-    var nav = document.querySelector('nav');
-    window.addEventListener('scroll', function() {
-      if (window.pageYOffset > 100) {
-        nav.classList.add('bg-dark', 'shadow');
-      } else {
-        nav.classList.remove('bg-dark', 'shadow');
-      }
-    });
-  </script>
+    <!--================ start footer Area  =================-->
+    <footer class="footer-area section_gap" id="contact">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-3  col-md-6 col-sm-6">
+                    <div class="single-footer-widget">
+                        <h6 class="footer_title">About Agency</h6>
+                        <p>The world has become so fast paced that people don’t want to stand by reading a page of information, they would much rather look at a presentation and understand the message. It has come to a point </p>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6 col-sm-6">
+                    <div class="single-footer-widget">
+                        <h6 class="footer_title">Navigation Links</h6>
+                        <div class="row">
+                            <div class="col-4">
+                                <ul class="list_style">
+                                    <li><a href="#">Home</a></li>
+                                    <li><a href="#">Feature</a></li>
+                                    <li><a href="#">Services</a></li>
+                                    <li><a href="#">Portfolio</a></li>
+                                </ul>
+                            </div>
+                            <div class="col-4">
+                                <ul class="list_style">
+                                    <li><a href="#">Team</a></li>
+                                    <li><a href="#">Pricing</a></li>
+                                    <li><a href="#">Blog</a></li>
+                                    <li><a href="#">Contact</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6 col-sm-6">
+                    <div class="single-footer-widget">
+                        <h6 class="footer_title">Newsletter</h6>
+                        <p>For business professionals caught between high OEM price and mediocre print and graphic output, </p>
+                        <div id="mc_embed_signup">
+                            <form target="_blank" action="https://spondonit.us12.list-manage.com/subscribe/post?u=1462626880ade1ac87bd9c93a&amp;id=92a4423d01" method="get" class="subscribe_form relative">
+                                <div class="input-group d-flex flex-row">
+                                    <input name="EMAIL" placeholder="Email Address" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Email Address '" required="" type="email">
+                                    <button class="btn sub-btn"><span class="lnr lnr-location"></span></button>
+                                </div>
+                                <div class="mt-10 info"></div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6 col-sm-6">
+                    <div class="single-footer-widget instafeed">
+                        <h6 class="footer_title">InstaFeed</h6>
+                        <ul class="list_style instafeed d-flex flex-wrap">
+                            <li><img src="image/instagram/Image-01.jpg" alt=""></li>
+                            <li><img src="image/instagram/Image-02.jpg" alt=""></li>
+                            <li><img src="image/instagram/Image-03.jpg" alt=""></li>
+                            <li><img src="image/instagram/Image-04.jpg" alt=""></li>
+                            <li><img src="image/instagram/Image-05.jpg" alt=""></li>
+                            <li><img src="image/instagram/Image-06.jpg" alt=""></li>
+                            <li><img src="image/instagram/Image-07.jpg" alt=""></li>
+                            <li><img src="image/instagram/Image-08.jpg" alt=""></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="border_line"></div>
+            <div class="row footer-bottom d-flex justify-content-between align-items-center">
+                <p class="col-lg-8 col-sm-12 footer-text m-0">
+                    <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+                    Copyright &copy;<script>
+                        document.write(new Date().getFullYear());
+                    </script> All rights reserved | This template is made with <i class="fa fa-heart-o" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
+                    <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+                </p>
+                <div class="col-lg-4 col-sm-12 footer-social">
+                    <a href="#"><i class="fa fa-facebook"></i></a>
+                    <a href="#"><i class="fa fa-twitter"></i></a>
+                    <a href="#"><i class="fa fa-dribbble"></i></a>
+                    <a href="#"><i class="fa fa-behance"></i></a>
+                </div>
+            </div>
+        </div>
+    </footer>
+    <!--================ End footer Area  =================-->
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="js/jquery-3.2.1.min.js"></script>
+    <script src="js/popper.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="vendors/owl-carousel/owl.carousel.min.js"></script>
+    <script src="js/jquery.ajaxchimp.min.js"></script>
+    <script src="js/mail-script.js"></script>
+    <script src="vendors/bootstrap-datepicker/bootstrap-datetimepicker.min.js"></script>
+    <script src="vendors/nice-select/js/jquery.nice-select.js"></script>
+    <script src="js/mail-script.js"></script>
+    <script src="js/stellar.js"></script>
+    <script src="vendors/lightbox/simpleLightbox.min.js"></script>
+    <script src="js/custom.js"></script>
 </body>
 
 </html>
